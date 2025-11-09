ad55b298963d68cdb1b37920bbed40f5fb4af72f
Yuanzhe (Anthony) Chen <yuanzhe.chen@student.unsw.edu.au>
2025-11-09 12:37:54 +1100

    Update fibonacci_number.cpp

--- Fibonacci Number/fibonacci_number.cpp
+++ Fibonacci Number/fibonacci_number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1) return n;      // 基本情况：F(0)=0, F(1)=1
-        return fib(n - 1) + fib(n - 2);  // 递归公式
+        if (n <= 1) return n;      // Base cases: F(0) = 0, F(1) = 1
+        return fib(n - 1) + fib(n - 2);  // Recursive Formula
     }
 };
